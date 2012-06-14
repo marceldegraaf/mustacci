a2f621c7b5654ee548355a5939664682436596b0
Marcel de Graaf <mail@marceldegraaf.net>
2012-06-14 22:47:37 +0200

    Add default layout and change :public to :public_folder to prevent deprecation warnings

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'compass'
 require 'sinatra'
 require 'slim'
 
-set :app_file, __FILE__
-set :root,    File.dirname(__FILE__)
-set :views,   'views'
-set :public,  'public'
+set :app_file,       __FILE__
+set :root,           File.dirname(__FILE__)
+set :views,          'views'
+set :public_folder,  'public'
 
 configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
