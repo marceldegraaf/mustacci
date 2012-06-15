838fe83b888b5549869a9c0d4d8a86ee989bb805
Marcel de Graaf <mail@marceldegraaf.net>
2012-06-15 09:45:49 +0200

    Start working on a layout

--- app.rb
+++ app.rb
@@ -3,15 +3,19 @@ require 'compass'
 require 'sinatra'
 require 'slim'
 
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+end
+
 set :app_file,       __FILE__
 set :root,           File.dirname(__FILE__)
 set :views,          'views'
 set :public_folder,  'public'
 
-configure do
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
-end
-
 get '/' do
   slim :index
+end
+
+get '/stylesheets/main.css' do
+  sass :main, Compass.sass_engine_options
 end
